3db155891f9e413a7f778e20464c5e716d76f2d8
Hardik Gupta <119853431+Hardik0702@users.noreply.github.com>
2025-12-28 22:15:36 +0000

    Variables_basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountName = "Hardik"; 
+let accountNumber = 123456;
+var accountPass = "12qw34er";
+accountState = "Uttarpradesh";
+
+// accountCity = Kanpur;    -------> Strings in inverted commas.
+
+let pinCode;
+
+// accountName = "Dickhard" -----> Cannot chnaged the values of const
+
+accountNumber = 987654
+accountPass = "m0n9b8"
+accountState = "DElhi"
+
+/*
+Use let instead of var because of block scope and functional scopes problems.
+*/
+
+console.table([accountName, accountNumber, accountPass, accountState, pinCode])
