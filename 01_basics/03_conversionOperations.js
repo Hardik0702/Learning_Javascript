ea7faa1c1f76de098af0740a6136760429beb194
Hardik Gupta <119853431+Hardik0702@users.noreply.github.com>
2025-12-29 23:27:39 +0000

    Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,70 @@
+let score1 = 33
+// console.log(typeof score1);
+
+let number1 = Number(score1)
+// console.log(typeof(number1));
+// console.log(number1)
+
+
+// 33 ---> 33
+// "33"-----> 33
+// "33aa"---->NaN
+// true----> 1
+// false----->0
+// null------>0
+// undefined---> NaN
+
+
+let someNumber = "Hardik"
+
+let someBoolean = Boolean(someNumber)
+// console.log(someBoolean);
+
+// 1----> true
+// ""----->false
+// "Hardik"----> true
+
+
+let number = 33
+
+let stringNumber = String(number)
+// console.log(stringNumber);
+
+// --------------------------Operations-------------------------------------------
+
+let value =3
+let negative = -value
+// console.log(negative);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+
+let str1 = "Hardik"
+let str2 = "Gupta"
+
+// console.log(str1 + str2);
+
+// console.log(1 + "2"); ---------->12
+// console.log("1" + 2); ----------->12
+// console.log("1" + 2 + 2);--------->122
+// console.log(1 + 2 + "2");--------->32
+
+// console.log(+true);------->1
+// console.log(+"");--------->0
+
+// prefix and postfix concept also
+
+
+
+
+
+
+
+
+
