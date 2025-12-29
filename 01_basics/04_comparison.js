cb4f8c3b940f3a3a7299a17b10a24f0c3f9ef112
Hardik Gupta <119853431+Hardik0702@users.noreply.github.com>
2025-12-29 23:42:24 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1)
+// console.log("02" > 1);
+
+// console.log(null > 0);         //comparison(>,<,>=,<=) and equality(==) works differently
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log("2" === 2);    // (===)---> it is strict check, whick checks datatypes also
+
+
+
