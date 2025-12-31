7bb8b51e3af042355ddaf83ea1dabe282ed9bb2a
Hardik Gupta <119853431+Hardik0702@users.noreply.github.com>
2025-12-31 07:49:29 +0000

    memory understanding

---
+++ 01_basics/05_memory.js
@@ -0,0 +1,24 @@
+// Stack (Primitive Datatypes),  Heap (Non-primitive Datatypes)
+
+let myName = "Hardik"
+
+let anotherName = myName
+anotherName="gupta"
+
+// console.log(myName);
+// console.log(anotherName);
+
+
+let user = {
+    email: "abc@gmail.com",
+    upi: "qwer@ok"
+}
+
+let user2 = user
+
+user.email = "123@gmail.com"
+
+// console.log(user.email);
+// console.log(user2.email);
+
+
